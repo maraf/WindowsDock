c4d84925ca9a2d2af7a03b9dd34f5361402c0e20
ing.smm@gmail.com <ing.smm@gmail.com@e9114fb4-9265-9f73-b7ac-e221a392d6e7>
2012-04-24 16:04:51 +0000

    Shortcut focus visual state

--- WindowsDock.GUI/App_Code/Version.cs
+++ WindowsDock.GUI/App_Code/Version.cs
@@ -7,8 +7,8 @@ namespace WindowsDock.GUI
 {
     public class Version
     {
-        public static readonly System.Version Current = new System.Version(1, 2, 21);
+        public static readonly System.Version Current = new System.Version(1, 2, 22);
 
-        public static readonly DateTime BuildDate = new DateTime(2012, 2, 7);
+        public static readonly DateTime BuildDate = new DateTime(2012, 4, 24);
     }
 }
